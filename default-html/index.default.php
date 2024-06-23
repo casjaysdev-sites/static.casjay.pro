5f19b0cdd53546ba791fb7dbe269de0e59eb2cfd
casjay <git-admin@casjaysdev.pro>
2024-06-23 10:42:38 -0400

    🗃️ Committing everything that changed 🗃️
    index.default.php

--- default-html/index.default.php
+++ default-html/index.default.php
@@ -19,13 +19,15 @@ site owner is working on it!  <br><br><br><br>
 
   <div class="c3">
 Server Admin you can now upload your site to  <br>
-<?php echo $_SERVER['DOCUMENT_ROOT']; ?>  <br><br><br>
-<?php echo "Server Name: " . $_SERVER['SERVER_NAME'] . "<BR>"; ?>
-<?php echo "IP Address: " . $_SERVER['SERVER_ADDR'] . "<BR>"; ?>  <br>
-Linux OsVer: <?php echo shell_exec('cat /etc/redhat-release'); ?>  <br>
-ConfigVer: <?php echo shell_exec(
-  'cat /etc/casjaysdev/updates/versions/configs.txt'
-); ?>
+<?php echo $_SERVER['DOCUMENT_ROOT']; ?>
+<br><br><br>
+<?php echo "System Hostname: " , gethostname() . "<br />"; ?>
+<?php echo "Server Name: " . $_SERVER['SERVER_NAME'] . "<br />"; ?>
+<?php echo "IP Address: " . $_SERVER['SERVER_ADDR'] . "<br />"; ?>
+<br>
+Linux OsVer: <?php echo shell_exec('cat /etc/redhat-release'); ?>
+<br>
+ConfigVer: <?php echo shell_exec('cat /etc/casjaysdev/updates/versions/configs.txt'); ?>
 <br><br><br>
 Powered by a Redhat based system<br>
 <a href="https://redhat.com"> <img border="0" alt="Redhat/CentOS/Fedora/SL Linux" src="/default-icons/powered_by_redhat.jpg"> </a><br><br><br><br>
